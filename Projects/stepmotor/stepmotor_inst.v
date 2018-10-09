c24898ad8e5060798bf33b01fc0d6d8654a4bd40
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-10-09 09:27:09 -0300

    Remove auto generated files from remote

--- Projects/stepmotor/stepmotor_inst.v
+++
@@ -1,7 +0,0 @@
-	stepmotor u0 (
-		.clk_clk            (<connected-to-clk_clk>),            //         clk.clk
-		.leds_export        (<connected-to-leds_export>),        //        leds.export
-		.reset_reset_n      (<connected-to-reset_reset_n>),      //       reset.reset_n
-		.stepmotor_1_export (<connected-to-stepmotor_1_export>)  // stepmotor_1.export
-	);
-
